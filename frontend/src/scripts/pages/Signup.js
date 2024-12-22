c41291a607d4943467f9baccb764a5ab77a7d453
relystrix <vianharyadi4072@gmail.com>
2024-12-22 17:57:41 +0700

    Signup.js

--- frontend/src/scripts/pages/Signup.js
+++ frontend/src/scripts/pages/Signup.js
@@ -1,36 +1,36 @@
 const Signup = {
   async render() {
     return `
-        <div class="signup-wrapper">
-          <section class="container-signup">
-            <div class="left-section">
-              <h1>Selamat Datang<br>di <span>NutriGuide!</span></h1>
-              <img src="./images/heroes/signup-login.webp" alt="Signup Image">
-            </div>
-            <div class="right-section">
-              <h2>Daftar</h2>
-              <form id="signup-form">
-                <div class="input-group">
-                  <input type="text" placeholder="Username" required>
-                </div>
-                <div class="input-group">
-                  <input type="email" placeholder="Email" required>
-                </div>
-                <div class="input-group">
-                  <input type="password" placeholder="Kata Sandi" required>
-                </div>
-                <div class="input-group">
-                  <input type="password" placeholder="Konfirmasi Kata Sandi" required>
-                </div>
-                <div class="input-group">
-                  <button class="button-signup" type="submit">Daftar</button>
-                </div>
-                <p class="login">Sudah punya akun? <a href="#" id="go-to-login">Masuk</a></p>
-              </form>
-            </div>
-          </section>
-        </div>
-      `;
+      <div class="signup-wrapper">
+        <section class="container-signup">
+          <div class="left-section">
+            <h1>Selamat Datang<br>di <span>NutriGuide!</span></h1>
+            <img src="./images/heroes/signup-login.webp" alt="Signup Image">
+          </div>
+          <div class="right-section">
+            <h2>Daftar</h2>
+            <form id="signup-form">
+              <div class="input-group">
+                <input type="text" placeholder="Username" required>
+              </div>
+              <div class="input-group">
+                <input type="email" placeholder="Email" required>
+              </div>
+              <div class="input-group">
+                <input type="password" placeholder="Kata Sandi" required>
+              </div>
+              <div class="input-group">
+                <input type="password" placeholder="Konfirmasi Kata Sandi" required>
+              </div>
+              <div class="input-group">
+                <button class="button-signup" type="submit">Daftar</button>
+              </div>
+              <p class="login">Sudah punya akun? <a href="#" id="go-to-login">Masuk</a></p>
+            </form>
+          </div>
+        </section>
+      </div>
+    `;
   },
 
   async afterRender() {
@@ -45,6 +45,7 @@ const Signup = {
     const signupForm = document.getElementById('signup-form');
     signupForm.addEventListener('submit', (event) => {
       event.preventDefault();
+
       window.location.hash = '/home';
     });
 
