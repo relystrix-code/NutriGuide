17e49ef736e85c1a17acf11e9c1aee2bc1bbb47e
relystrix <vianharyadi4072@gmail.com>
2024-12-22 17:36:25 +0700

    Login.js & Login.scss

--- frontend/src/scripts/pages/Login.js
+++ frontend/src/scripts/pages/Login.js
@@ -1,28 +1,28 @@
 const Login = {
   async render() {
     return `
-        <div class="login-wrapper">
-          <section class="container-login">
-            <div class="left-section">
-              <h1>Selamat Datang<br>di <span>NutriGuide!</span></h1>
-              <img src="./images/heroes/signup-login.webp" alt="Login Image">
-            </div>
-            <div class="right-section">
-              <h2>Masuk</h2>
-              <form id="login-form">
-                <div class="input-group">
-                  <input type="email" id="email" placeholder="Email" required>
-                </div>
-                <div class="input-group">
-                  <input type="password" id="password" placeholder="Kata Sandi" required>
-                </div>
-                <button class="button-login" type="submit">Masuk</button>
-                <p class="register">Belum punya akun? <a href="#">Daftar Sekarang</a></p>
-              </form>
-            </div>
-          </section>
-        </div>
-      `;
+      <div class="login-wrapper">
+        <section class="container-login">
+          <div class="left-section">
+            <h1>Selamat Datang<br>di <span>NutriGuide!</span></h1>
+            <img src="./images/heroes/signup-login.webp" alt="Login Image">
+          </div>
+          <div class="right-section">
+            <h2>Masuk</h2>
+            <form id="login-form">
+              <div class="input-group">
+                <input type="email" id="email" placeholder="Email" required>
+              </div>
+              <div class="input-group">
+                <input type="password" id="password" placeholder="Kata Sandi" required>
+              </div>
+              <button class="button-login" type="submit">Masuk</button>
+              <p class="register">Belum punya akun? <a href="#">Daftar Sekarang</a></p>
+            </form>
+          </div>
+        </section>
+      </div>
+    `;
   },
 
   async afterRender() {
@@ -30,12 +30,10 @@ const Login = {
 
     const header = document.getElementById('main-header');
     const footer = document.getElementById('main-footer');
-
     if (header) header.style.display = 'none';
     if (footer) footer.style.display = 'none';
 
     const loginForm = document.getElementById('login-form');
-
     loginForm.addEventListener('submit', (event) => {
       event.preventDefault();
 
