85f98a268bf5b6f82efef72f6338aa43e568f5c1
relystrix <vianharyadi4072@gmail.com>
2024-12-22 16:29:48 +0700

    Articles.js & Articles.scss

--- frontend/src/scripts/pages/Articles.js
+++ frontend/src/scripts/pages/Articles.js
@@ -3,25 +3,26 @@ import Endpoints from '../api/endpoints';
 const Articles = {
   async render() {
     return `
-        <section class="hero-article">
-          <div class="hero-content-article">
-            <h1>NutriGuide</h1>
-            <p>Temukan artikel pilihan dan jadikan hari Anda lebih bermakna!</p>
-            <img src="./images/heroes/article-page.webp" alt="Hero Image"> 
-          </div>
-        </section>
-        <section class="artikel-section">
-          <h2>Artikel</h2>
-          <div class="artikel-grid" id="artikel-grid"></div>
-        </section>
-      `;
+      <section class="hero-article">
+        <div class="hero-content-article">
+          <h1>NutriGuide</h1>
+          <p>Temukan artikel pilihan dan jadikan hari Anda lebih bermakna!</p>
+          <img src="./images/heroes/article-page.webp" alt="Hero Image"> 
+        </div>
+      </section>
+      <section class="artikel-section">
+        <h2>Artikel</h2>
+        <div class="artikel-grid" id="artikel-grid"></div>
+      </section>
+    `;
   },
 
   async afterRender() {
+    const artikelGrid = document.getElementById('artikel-grid');
+
     try {
       const response = await Endpoints.AllArticles();
       const articles = response.data.articles;
-      const artikelGrid = document.getElementById('artikel-grid');
 
       if (!articles || articles.length === 0) {
         artikelGrid.innerHTML = '<p>Tidak ada artikel tersedia.</p>';
@@ -29,25 +30,27 @@ const Articles = {
       }
 
       artikelGrid.innerHTML = articles
-        .map(
-          (article) => `
-          <a href="#/articles/${article.id}">
-            <div class="artikel-card">
-              <img src="${article.picture}" alt="${article.title}">
-              <h3>${article.title}</h3>
-              <p>${article.category} | ${new Date(
-  article.date
-).toLocaleDateString('id-ID')}</p>
-            </div>
-          </a>
-        `
-        )
+        .map((article) => this.createArticleCard(article))
         .join('');
     } catch (error) {
       console.error('Gagal memuat artikel:', error);
-      const artikelGrid = document.getElementById('artikel-grid');
-      artikelGrid.innerHTML = '<p>Gagal memuat artikel.</p>';
+      artikelGrid.innerHTML =
+        '<p>Gagal memuat artikel. Silakan coba lagi nanti.</p>';
     }
+  },
+
+  createArticleCard(article) {
+    return `
+      <a href="#/articles/${article.id}" class="artikel-link">
+        <div class="artikel-card">
+          <img src="${article.picture}" alt="${article.title}">
+          <h3>${article.title}</h3>
+          <p>${article.category} | ${new Date(article.date).toLocaleDateString(
+  'id-ID'
+)}</p>
+        </div>
+      </a>
+    `;
   },
 };
 
